c3705c342f6f43d7825278d91c561353f71fc3ef
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2017-12-28 19:42:37 +0200

    Implement lib without prisms (#1)
    * Starting to implement things without prisms
    
    * Finishing implementation of tests
    
    * Do not depend on FsUnit as the matching does not seem to hold

--- test/Tests/Tests.fs
+++ test/Tests/Tests.fs
@@ -3,8 +3,9 @@ module Tests
 open System
 open Xunit
 open FSharpPlus
-open FsUnit
+open FSharpPlus.Compatibility.Haskell
 open Validations
+open Validations.Validations // not good
 
 //( # ) :: AReview t b -> b -> t
 let seven = 7
@@ -15,19 +16,74 @@ let plusOne x = x + 1
 let testYY() =
   let subject:AccValidation<string,int>  = AccSuccess plusOne <*> AccSuccess seven
   let expected = AccSuccess 8
-  Assert.Equal(expected,subject)
+  Assert.Equal(expected, subject)
 [<Fact>]
 let testNY() =
   let subject:AccValidation<string list,int>  = AccFailure ["f1"] <*> AccSuccess seven
   let expected = AccFailure ["f1"]
-  Assert.Equal(expected,subject)
+  Assert.Equal(expected, subject)
 [<Fact>]
 let testYN() =
   let subject:AccValidation<string list,int>  = AccSuccess plusOne <*> AccFailure ["f2"] 
   let expected = AccFailure ["f2"]
-  Assert.Equal(expected,subject)
+  Assert.Equal(expected, subject)
 [<Fact>]
 let testNN() =
   let subject:AccValidation<string list,int>  = AccFailure ["f1"] <*> AccFailure ["f2"] 
   let expected = AccFailure ["f1";"f2"]
-  Assert.Equal(expected,subject)
+  Assert.Equal(expected, subject)
+(*
+[<Fact>]
+let testValidationNel() =
+  let subject  = validation length (const' 0) $ validationNel (Left ())
+  subject |> should equal 1
+*)
+[<Fact>]
+let testEnsureLeftFalse () =
+  let subject = ensure three (const' false) (AccFailure seven)
+  Assert.Equal((AccFailure seven), subject)
+
+[<Fact>]
+let testEnsureLeftTrue () =
+  let subject = ensure three (const' true) (AccFailure seven)
+  Assert.Equal((AccFailure seven), subject)
+
+[<Fact>]
+let testEnsureRightFalse () =
+  let subject = ensure three (const' false) (AccSuccess seven)
+  Assert.Equal((AccFailure three), subject)
+
+[<Fact>]
+let testEnsureRightTrue () =
+  let subject = ensure three (const' true ) (AccSuccess seven)
+  Assert.Equal((AccSuccess seven), subject)
+
+[<Fact>]
+let testOrElseRight () =
+  let v = AccSuccess  seven
+  let subject = orElse v three
+  Assert.Equal(seven, subject)
+
+[<Fact>]
+let testOrElseLeft () =
+  let v = AccFailure seven
+  let subject = orElse v three
+  Assert.Equal(three, subject)
+
+//testEnsureLeftFalse, testEnsureLeftTrue, testEnsureRightFalse, testEnsureRightTrue,
+//  testOrElseRight, testOrElseLeft
+//  :: forall v. (Validate v, Eq (v Int Int), Show (v Int Int)) => Proxy v -> Test
+
+
+[<Fact>]
+let testValidateTrue ()=
+  let subject = validate three (const' true) seven
+  let expected = AccSuccess seven
+  Assert.Equal(expected, subject)
+
+[<Fact>]
+let testValidateFalse ()=
+  let subject = validate three (const' true) seven
+  let expected = AccFailure three
+  Assert.Equal(expected, subject)
+
