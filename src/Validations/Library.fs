eba5ebd9cba10080811526bf99a7e0072efd15e2
Oskar Gewalli <oskar@gewalli.se>
2017-12-28 18:10:38 +0200

    Starting to implement things without prisms

--- src/Validations/Library.fs
+++ src/Validations/Library.fs
@@ -17,44 +17,88 @@ open FSharpPlus.Compatibility.Haskell
 type AccValidation<'err,'a> =
   | AccFailure of 'err
   | AccSuccess of 'a
-[<AutoOpen>]
-module AccValidation=
-  let fmap f = function 
-      | AccFailure e -> AccFailure e
-      | AccSuccess a -> AccSuccess (f a)
 
-//instance Semigroup err => Apply (AccValidation err) where
-  let inline (<*>) e1' e2' = 
+module AccValidation=
+  let inline map (f:'T->'U) x=
+      match x with 
+      | AccFailure e -> AccFailure e
+      | AccSuccess a -> AccSuccess (f a) 
+  let inline apply e1' e2' = 
     match e1',e2' with
     | AccFailure e1, AccFailure e2 -> AccFailure (plus e1 e2)
     | AccFailure e1, AccSuccess _  -> AccFailure e1
     | AccSuccess _, AccFailure e2 -> AccFailure e2
     | AccSuccess f, AccSuccess a -> AccSuccess (f a)
-
-  //let inline (<*>) = (<.>)
-
-  let inline (<!>) a' x' = 
-    match a',x' with
-    | AccFailure _ , x -> x
-    | AccSuccess a , _ ->AccSuccess a
-
   let inline foldr f x = function 
       | (AccSuccess a) -> f a x
       | (AccFailure _) -> x
 
   let inline traverse f = function 
-    | (AccSuccess a) -> AccSuccess (fmap f a)
+    | (AccSuccess a) -> AccSuccess (map f a)
     | (AccFailure e) -> AccSuccess (AccFailure e)
 
-  let bimap f g = function
+  let inline bimap f g = function
     | (AccFailure e) -> AccFailure (f e)
     | (AccSuccess a) -> AccSuccess (g a)
 
-  let bifoldr f g x = function 
+  let inline bifoldr f g x = function 
       |(AccSuccess a) -> g a x
       |(AccFailure e) -> f e x
 
-  let bitraverse f g = function 
-      | (AccSuccess a) -> AccSuccess (fmap g a)
-      | (AccFailure e) -> AccFailure (fmap f e)
+  let inline bitraverse f g = function 
+      | (AccSuccess a) -> AccSuccess (map g a)
+      | (AccFailure e) -> AccFailure (map f e)
 
+  let inline bind (f:'T->AccValidation<_,_>) x :AccValidation<_,_>=
+      match x with 
+      | AccFailure e -> AccFailure e
+      | AccSuccess a -> (f a) 
+
+
+
+type AccValidation<'err,'a> with
+
+  static member inline (<*>)      (e1':AccValidation<'Monoid,_>, e2':AccValidation<'Monoid,_>) : AccValidation<'Monoid,_> = AccValidation.apply e1' e2'
+  static member inline Map        (x : AccValidation<_,_>, f) = AccValidation.map f x
+  static member inline Bind       (x, f)     = AccValidation.bind f x
+ 
+
+module Validations=
+  let liftResult (f:('b -> 'e)) : (Result<'a,'b>->AccValidation<'e,'a>) = function | Error e-> AccFailure (f e) | Ok a-> AccSuccess a
+  /// | 'liftError' is useful for converting an 'Either' to an 'AccValidation'
+  /// when the @Left@ of the 'Either' needs to be lifted into a 'Semigroup'.
+  let liftChoice (f:('b -> 'e)) : (Either<'b,'a>->AccValidation<'e,'a>) = either (AccFailure << f) AccSuccess
+
+  /// | 'validate's the @a@ with the given predicate, returning @e@ if the predicate does not hold.
+  ///
+  /// This can be thought of as having the less general type:
+  ///
+  /// @
+  /// validate :: e -> (a -> Bool) -> a -> AccValidation e a
+  /// @
+  let validate (e:'e) (p:('a -> bool)) (a:'a) : AccValidation<'e,'a> = if p a then AccSuccess a else AccFailure e
+
+  /// | @v 'orElse' a@ returns @a@ when @v@ is AccFailure, and the @a@ in @AccSuccess a@.
+  ///
+  /// This can be thought of as having the less general type:
+  ///
+  /// @
+  /// orElse :: AccValidation e a -> a -> a
+  /// @
+  let inline orElse v a = 
+      match v with
+      | AccFailure _ -> a
+      | AccSuccess x -> x
+
+  /// | 'ensure' leaves the validation unchanged when the predicate holds, or
+  /// fails with @e@ otherwise.
+  ///
+  /// This can be thought of as having the less general type:
+  ///
+  /// @
+  /// ensure :: e -> (a -> Bool) -> AccValidation e a -> AccValidation e a
+  /// @
+  let inline ensure (e:'e) (p:'a-> bool) =
+    function
+    |AccFailure x -> AccFailure x
+    |AccSuccess a -> validate e p a
--- test/Tests/Tests.fs
+++ test/Tests/Tests.fs
@@ -3,8 +3,11 @@ module Tests
 open System
 open Xunit
 open FSharpPlus
+open FSharpPlus.Compatibility.Haskell
 open FsUnit
+open FsUnit.Xunit
 open Validations
+open Validations.Validations // not good
 
 //( # ) :: AReview t b -> b -> t
 let seven = 7
@@ -15,19 +18,75 @@ let plusOne x = x + 1
 let testYY() =
   let subject:AccValidation<string,int>  = AccSuccess plusOne <*> AccSuccess seven
   let expected = AccSuccess 8
-  Assert.Equal(expected,subject)
+  subject |> should equal expected
 [<Fact>]
 let testNY() =
   let subject:AccValidation<string list,int>  = AccFailure ["f1"] <*> AccSuccess seven
   let expected = AccFailure ["f1"]
-  Assert.Equal(expected,subject)
+  subject |> should equal expected
 [<Fact>]
 let testYN() =
   let subject:AccValidation<string list,int>  = AccSuccess plusOne <*> AccFailure ["f2"] 
   let expected = AccFailure ["f2"]
-  Assert.Equal(expected,subject)
+  subject |> should equal expected
 [<Fact>]
 let testNN() =
   let subject:AccValidation<string list,int>  = AccFailure ["f1"] <*> AccFailure ["f2"] 
   let expected = AccFailure ["f1";"f2"]
-  Assert.Equal(expected,subject)
+  subject |> should equal expected
+(*
+[<Fact>]
+let testValidationNel() =
+  let subject  = validation length (const' 0) $ validationNel (Left ())
+  subject |> should equal 1
+*)
+let testEnsureLeftFalse () =
+  let //subject :: v Int Int
+      subject = ensure three (const' false) (AccFailure seven)
+  subject |> should equal (AccFailure  seven)
+
+let testEnsureLeftTrue () =
+  let //subject :: v Int Int
+      subject = ensure three (const' true) (AccFailure seven)
+  subject |> should equal (AccFailure  seven)
+
+let testEnsureRightFalse () =
+  let //subject :: v Int Int
+      subject = ensure three (const' false) (AccSuccess seven)
+  subject |> should equal (AccFailure  three)
+
+let testEnsureRightTrue () =
+  let //subject :: v Int Int
+      subject = ensure three (const' true ) (AccSuccess seven)
+  subject |> should equal (AccSuccess seven)
+
+let testOrElseRight () =
+  let //v :: v Int Int
+      v = AccSuccess  seven
+  let subject = orElse v three
+  subject |> should equal seven
+//  in  TestCase (assertEqual "orElseRight" subject seven)
+
+let testOrElseLeft () =
+  let //v :: v Int Int
+      v = AccFailure seven
+  let subject = orElse v three
+  in  TestCase (assertEqual "orElseLeft" subject three)
+
+//testEnsureLeftFalse, testEnsureLeftTrue, testEnsureRightFalse, testEnsureRightTrue,
+//  testOrElseRight, testOrElseLeft
+//  :: forall v. (Validate v, Eq (v Int Int), Show (v Int Int)) => Proxy v -> Test
+
+
+//testValidateTrue :: Test
+let testValidateTrue ()=
+  let subject = validate three (const True) seven
+      expected = AccSuccess seven
+  in  TestCase (assertEqual "testValidateTrue" subject expected)
+
+//testValidateFalse :: Test
+let testValidateFalse ()=
+  let subject = validate three (const True) seven
+      expected = AccFailure three
+  in  TestCase (assertEqual "testValidateFalse" subject expected)
+
